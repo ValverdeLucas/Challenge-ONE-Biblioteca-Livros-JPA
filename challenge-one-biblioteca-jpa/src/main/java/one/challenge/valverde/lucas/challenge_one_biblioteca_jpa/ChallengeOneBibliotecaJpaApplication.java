735c991e482ec0b8c29cc8d4fe7866976e86ec1c
Lucas Valverde <valverdelucas95@gmail.com>
2024-07-02 13:52:05 -0300

    Commit de criação do repo

---
+++ challenge-one-biblioteca-jpa/src/main/java/one/challenge/valverde/lucas/challenge_one_biblioteca_jpa/ChallengeOneBibliotecaJpaApplication.java
@@ -0,0 +1,13 @@
+package one.challenge.valverde.lucas.challenge_one_biblioteca_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeOneBibliotecaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeOneBibliotecaJpaApplication.class, args);
+	}
+
+}
